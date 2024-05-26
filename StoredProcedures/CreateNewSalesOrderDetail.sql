34d513595ebbdb9f94aacdf65be345c99b39b12a
Algirdas Poskus <AlgirdasLeonardas@gmail.com>
2024-05-26 14:19:08 +0300

    Defined new stored procedure

---
+++ StoredProcedures/CreateNewSalesOrderDetail.sql
@@ -0,0 +1,10 @@
+CREATE PROCEDURE CreateNewSalesOrderDetail
+    @SalesOrderID INT,
+    @OrderQty INT,
+    @ProductID INT,
+    @UnitPrice DECIMAL(8,2)
+AS
+BEGIN
+    INSERT INTO SalesLT.SalesOrderDetail(SalesOrderID, OrderQty, ProductID, UnitPrice)
+    VALUES(@SalesOrderID, @OrderQty, @ProductID, @UnitPrice)
+END

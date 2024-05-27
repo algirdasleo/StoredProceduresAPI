365ae662d572ac42b1ed141379990e84eca282aa
Algirdas Poskus <AlgirdasLeonardas@gmail.com>
2024-05-27 19:56:11 +0300

    Added DI for ConnectionFactory and DbService

--- Program.cs
+++ Program.cs
@@ -1,8 +1,18 @@
+using API.Factories;
+using API.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<ConnectionFactory>(provider =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    return new ConnectionFactory(connectionString);
+}
+);
+builder.Services.AddSingleton<DbService>();
 
 var app = builder.Build();
 if (app.Environment.IsDevelopment())

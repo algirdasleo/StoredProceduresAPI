e00ec6226c43f0ed6b8d11d5d056becb0484d1a7
Algirdas Poskus <AlgirdasLeonardas@gmail.com>
2024-05-27 19:55:54 +0300

    Defined SQL Connection Factory

---
+++ Factories/ConnectionFactory.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace API.Factories
+{
+    public class ConnectionFactory
+    {
+        private readonly string _connectionString;
+
+        public ConnectionFactory(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public IDbConnection CreateConnection()
+        {
+            var connection = new SqlConnection(_connectionString);
+            connection.Open();
+            return connection;
+        }
+    }
+}

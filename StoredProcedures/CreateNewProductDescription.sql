ac0512bdf47def601c0b5712e04db50f9c59befd
Algirdas Poskus <AlgirdasLeonardas@gmail.com>
2024-06-03 19:32:45 +0300

    Added error handling

--- StoredProcedures/CreateNewProductDescription.sql
+++ StoredProcedures/CreateNewProductDescription.sql
@@ -1,7 +1,15 @@
 CREATE PROCEDURE CreateNewProductDescription
-    @ProductDescription NVARCHAR(400)
+    @ProductDescription NVARCHAR(400),
+    @ErrorMessage NVARCHAR(400) OUTPUT
 AS
 BEGIN   
+    DECLARE @ProductDescriptionId INT;
     INSERT INTO SalesLT.ProductDescription(Description)
     VALUES(@ProductDescription);
+    SET @ProductDescriptionId = SCOPE_IDENTITY();
+
+    IF @ProductDescriptionId IS NULL
+    BEGIN
+        SET @ErrorMessage = 'Failed to create new product description';
+    END
 END

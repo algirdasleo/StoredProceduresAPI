69bd1e400fb8548d5f5f11c40c20515be0ca2907
Algirdas Poskus <AlgirdasLeonardas@gmail.com>
2024-06-03 18:53:04 +0300

    Added ability to add address + error handling

--- StoredProcedures/CreateNewCustomer.sql
+++ StoredProcedures/CreateNewCustomer.sql
@@ -2,9 +2,38 @@ CREATE PROCEDURE CreateNewCustomer
     @FirstName dbo.Name,
     @LastName dbo.Name,
     @PasswordHash VARCHAR(128),
-    @PasswordSalt VARCHAR(10)
+    @PasswordSalt VARCHAR(10),
+    @AddressLine1 NVARCHAR(60), 
+    @AddressLine2 NVARCHAR(60),
+    @City NVARCHAR(30),
+    @StateProvince dbo.Name,
+    @CountryRegion dbo.Name,
+    @PostalCode NVARCHAR(15),
+    @ErrorMessage NVARCHAR(4000) OUTPUT -- Output parameter, to return an error message, if any
 AS
 BEGIN
+    DECLARE @CustomerID INT;
     INSERT INTO SalesLT.Customer (FirstName, LastName, PasswordHash, PasswordSalt)
     VALUES (@FirstName, @LastName, @PasswordHash, @PasswordSalt);
+    SET @CustomerID = SCOPE_IDENTITY();
+
+    IF @CustomerID IS NULL
+    BEGIN
+        SET @ErrorMessage = 'Failed to create new customer.';
+        RETURN;
+    END
+
+    DECLARE @AddressID INT;
+    INSERT INTO SalesLT.Address (AddressLine1, AddressLine2, City, StateProvince, CountryRegion, PostalCode, CustomerID)
+    VALUES (@AddressLine1, @AddressLine2, @City, @StateProvince, @CountryRegion, @PostalCode, @CustomerID);
+    SET @AddressID = SCOPE_IDENTITY();
+
+    IF @AddressID IS NULL
+    BEGIN
+        SET @ErrorMessage = 'Failed to create new customer address.';
+        RETURN;
+    END
+    
+    INSERT INTO SalesLT.CustomerAddress (CustomerID, AddressID)
+    VALUES (@CustomerID, @AddressID);
 END

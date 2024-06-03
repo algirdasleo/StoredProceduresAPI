cef78cf5e8cb27dc1edfbc086867e6b28353ceef
Algirdas Poskus <AlgirdasLeonardas@gmail.com>
2024-06-03 19:18:34 +0300

    Added error handling

--- StoredProcedures/CreateNewProductCategory.sql
+++ StoredProcedures/CreateNewProductCategory.sql
@@ -1,7 +1,16 @@
 CREATE PROCEDURE CreateNewProductCategory
-    @Name NVARCHAR(50)
+    @Name NVARCHAR(50),
+    @ErrorMessage NVARCHAR(4000) OUTPUT
 AS
 BEGIN
+    DECLARE @ProductCategoryID INT;
     INSERT SalesLT.ProductCategory (Name)
     Values (@Name)
+    SET @ProductCategoryID = SCOPE_IDENTITY();
+    
+    IF @ProductCategoryID IS NULL
+    BEGIN
+        SET @ErrorMessage = 'Failed to create new product category.';
+        RETURN;
+    END
 END

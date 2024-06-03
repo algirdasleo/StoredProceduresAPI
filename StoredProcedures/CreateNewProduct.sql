1125a41138e5fa511a0cdd7e45b6514bf12bc3d1
Algirdas Poskus <AlgirdasLeonardas@gmail.com>
2024-06-03 17:45:04 +0300

    Added an optional paramater ProductDescription with error handling

--- StoredProcedures/CreateNewProduct.sql
+++ StoredProcedures/CreateNewProduct.sql
@@ -1,8 +1,9 @@
 CREATE PROCEDURE CreateNewProduct
     @Name DBO.NAME,
     @ProductNumber NVARCHAR(25),
-    @ProductModelID INT,        -- optional parameter
-    @ProductCategoryID INT,     -- optional parameter
+    @ProductModelID INT,                -- optional parameter
+    @ProductCategoryID INT,             -- optional parameter
+    @ProductDescription NVARCHAR(4000), -- optional parameter
     @StandardCost MONEY,
     @ListPrice MONEY,
     @SellStartDate DATETIME,
@@ -31,6 +32,30 @@ BEGIN
         END
     END
 
+    IF @ProductDescription IS NOT NULL
+    BEGIN
+        IF EXISTS (SELECT 1 FROM ProductModelProductDescription WHERE ProductModelID = @ProductModelID)
+        BEGIN
+            IF @ErrorMessage IS NULL 
+                SET @ErrorMessage = 'Product Model already has a description.';
+            ELSE
+                SET @ErrorMessage = @ErrorMessage + ' Product Model already has a description.';
+        END
+    END
+
     INSERT INTO SalesLT.Product (Name, ProductNumber, ProductModelID, ProductCategoryID, StandardCost, ListPrice, SellStartDate)
     VALUES (@Name, @ProductNumber, @ProductModelID, @ProductCategoryID, @StandardCost, @ListPrice, @SellStartDate);
+
+    IF @ProductDescription IS NOT NULL
+    BEGIN
+        IF NOT EXISTS (SELECT ProductDescriptionID FROM SalesLT.ProductModelProductDescription WHERE ProductModelID = @ProductModelID)
+        BEGIN
+            DECLARE @ProductDescriptionID INT;
+            INSERT INTO SalesLT.ProductDescription (Description)
+            VALUES (@ProductDescription);
+            SET @ProductDescriptionID = SCOPE_IDENTITY();
+            INSERT INTO SalesLT.ProductModelProductDescription (ProductModelID, ProductDescriptionID)
+            VALUES (@ProductModelID, @ProductDescriptionID);
+        END
+    END
 END

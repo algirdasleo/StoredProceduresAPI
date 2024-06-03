1db8f3903be4ef90d5b9b54a6dfed72e7be1562e
Algirdas Poskus <AlgirdasLeonardas@gmail.com>
2024-06-03 16:55:08 +0300

    Added Product ModelID and CategoryID as optional parameters + checks if they exist, else returns -2, -3

--- StoredProcedures/CreateNewProduct.sql
+++ StoredProcedures/CreateNewProduct.sql
@@ -1,11 +1,29 @@
 CREATE PROCEDURE CreateNewProduct
     @Name DBO.NAME,
     @ProductNumber NVARCHAR(25),
+    @ProductModelID INT, -- optional parameter
+    @ProductCategoryID INT, -- optional parameter
     @StandardCost MONEY,
     @ListPrice MONEY,
     @SellStartDate DATETIME
 AS
 BEGIN
-    INSERT INTO SalesLT.Product (Name, ProductNumber, StandardCost, ListPrice, SellStartDate)
-    VALUES (@Name, @ProductNumber, @StandardCost, @ListPrice, @SellStartDate)
+    IF @ProductModelID IS NOT NULL
+    BEGIN
+        IF NOT EXISTS (SELECT 1 FROM SalesLT.ProductModel WHERE ProductModelID = @ProductModelID)
+        BEGIN
+            RETURN -2;
+        END
+    END
+
+    IF @ProductCategoryID IS NOT NULL
+    BEGIN
+        IF NOT EXISTS (SELECT 1 FROM SalesLT.ProductCategory WHERE ProductCategoryID = @ProductCategoryID)
+        BEGIN
+            RETURN -3;
+        END
+    END
+
+    INSERT INTO SalesLT.Product (Name, ProductNumber, ProductModelID, ProductCategoryID, StandardCost, ListPrice, SellStartDate)
+    VALUES (@Name, @ProductNumber, @ProductModelID, @ProductCategoryID, @StandardCost, @ListPrice, @SellStartDate);
 END

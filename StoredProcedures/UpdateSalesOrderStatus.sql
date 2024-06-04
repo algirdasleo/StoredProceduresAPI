d7126bf3d6bfb74cf503dfcc99fad79cf47f8e02
Algirdas Poskus <AlgirdasLeonardas@gmail.com>
2024-06-04 13:56:11 +0300

    Added error handling

--- StoredProcedures/UpdateSalesOrderStatus.sql
+++ StoredProcedures/UpdateSalesOrderStatus.sql
@@ -1,8 +1,14 @@
 CREATE PROCEDURE UpdateSalesOrderStatus
     @SalesOrderID int,
-    @Status tinyint
+    @Status tinyint,
+    @ErrorMessage NVARCHAR(4000) OUTPUT
 AS
 BEGIN
+    IF NOT EXISTS (SELECT 1 FROM SalesLT.SalesOrderHeader WHERE SalesOrderID = @SalesOrderID)
+    BEGIN
+        SET @ErrorMessage = 'Sales order does not exist.';
+        RETURN;
+    END
     UPDATE SalesLT.SalesOrderHeader
     SET Status = @Status
     WHERE SalesOrderID = @SalesOrderID;

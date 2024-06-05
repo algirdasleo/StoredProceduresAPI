f471f9fd8fe9dbaca64cb60a853ad10fc5eeba8d
Algirdas Poskus <AlgirdasLeonardas@gmail.com>
2024-06-05 11:39:53 +0300

    Simplified if statements

--- StoredProcedures/CreateNewSalesOrderHeader.sql
+++ StoredProcedures/CreateNewSalesOrderHeader.sql
@@ -8,45 +8,16 @@ CREATE PROCEDURE CreateNewSalesOrderHeader
 AS
 BEGIN
     IF NOT EXISTS (SELECT 1 FROM SalesLT.Customer WHERE CustomerID = @CustomerID)
-    BEGIN
-        IF @ErrorMessage IS NULL
-        BEGIN
-            SET @ErrorMessage = 'Customer does not exist.';
-        END
-        ELSE 
-        BEGIN
-            SET @ErrorMessage = @ErrorMessage + ' Customer does not exist.';
-        END
-    END
+        SET @ErrorMessage = COALESCE(@ErrorMessage + ' ', '') + 'Customer does not exist.';
 
     IF @ShipToAddressID IS NOT NULL AND NOT EXISTS (SELECT 1 FROM SalesLT.Address WHERE AddressID = @ShipToAddressID)
-    BEGIN
-        IF @ErrorMessage IS NULL
-        BEGIN
-            SET @ErrorMessage = 'Shipping address does not exist.';
-        END
-        ELSE
-        BEGIN
-            SET @ErrorMessage = @ErrorMessage + ' Shipping address does not exist.';
-        END
-    END
+        SET @ErrorMessage = COALESCE(@ErrorMessage + ' ', '') + 'Shipping address does not exist.';
 
     IF @BillToAddressID IS NOT NULL AND NOT EXISTS (SELECT 1 FROM SalesLT.Address WHERE AddressID = @BillToAddressID)
-    BEGIN
-        IF @ErrorMessage IS NULL
-        BEGIN
-            SET @ErrorMessage = 'Billing address does not exist.';
-        END
-        ELSE
-        BEGIN
-            SET @ErrorMessage = @ErrorMessage + ' Billing address does not exist.';
-        END
-    END
+        SET @ErrorMessage = COALESCE(@ErrorMessage + ' ', '') + 'Billing address does not exist.';
 
     IF @ErrorMessage IS NOT NULL
-    BEGIN
         RETURN;
-    END
 
     INSERT INTO SalesLT.SalesOrderHeader (DueDate, CustomerID, ShipToAddressID, BillToAddressID, ShipMethod)
     VALUES (@DueDate, @CustomerID, @ShipToAddressID, @BillToAddressID, @ShipMethod);

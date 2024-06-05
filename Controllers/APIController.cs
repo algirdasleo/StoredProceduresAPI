cb46ac71b5a071218927b1d31fe356bc87bf1c04
Algirdas Poskus <AlgirdasLeonardas@gmail.com>
2024-06-05 22:44:22 +0300

    updated API controller with error messages

--- Controllers/APIController.cs
+++ Controllers/APIController.cs
@@ -16,52 +16,76 @@ namespace API.Controllers
             _dbService = dbService;
         }
 
+        [HttpGet("GetProductById/{id}")]
+        public async Task<IActionResult> GetProductById(int id)
+        {
+            var (products, ErrorMessage) = await _dbService.GetProductById(id);
+            if (ErrorMessage != null)
+                return BadRequest(ErrorMessage);
+            return Ok(products);
+        }
+        
         [HttpPost("CreateNewCustomer")]
         public async Task<IActionResult> CreateNewCustomer(Customer customer)
         {
-            var result = await _dbService.CreateNewCustomer(customer);
-            return Ok(result);
+            var (rowsAffected, ErrorMessage) = await _dbService.CreateNewCustomer(customer);
+            if (ErrorMessage != null)
+                return BadRequest(ErrorMessage);
+            return Ok(rowsAffected);
         }
 
         [HttpPost("CreateNewProduct")]
         public async Task<IActionResult> CreateNewProduct(Product product)
         {
-            var result = await _dbService.CreateNewProduct(product);
-            return Ok(result);
+            var (rowsAffected, ErrorMessage) = await _dbService.CreateNewProduct(product);
+            if (ErrorMessage != null)
+                return BadRequest(ErrorMessage);
+            return Ok(rowsAffected);
         }
 
         [HttpPost("CreateNewProductCategory")]
         public async Task<IActionResult> CreateNewProductCategory(ProductCategory productCategory)
         {
-            var result = await _dbService.CreateNewProductCategory(productCategory);
-            return Ok(result);
+            var (rowsAffected, ErrorMessage) = await _dbService.CreateNewProductCategory(productCategory);
+            if (ErrorMessage != null)
+                return BadRequest(ErrorMessage);
+            return Ok(rowsAffected);
         }
 
         [HttpPost("CreateNewProductDescription")]
         public async Task<IActionResult> CreateNewProductDescription(ProductDescription productDescription)
         {
-            var result = await _dbService.CreateNewProductDescription(productDescription);
-            return Ok(result);
+            var (rowsAffected, ErrorMessage) = await _dbService.CreateNewProductDescription(productDescription);
+            if (ErrorMessage != null)
+                return BadRequest(ErrorMessage);
+            return Ok(rowsAffected);
         }
 
         [HttpPost("CreateNewSalesOrderDetail")]
         public async Task<IActionResult> CreateNewSalesOrderDetail(SalesOrderDetail salesOrderDetail)
         {
-            var result = await _dbService.CreateNewSalesOrderDetail(salesOrderDetail);
-            return Ok(result);
+            var (rowsAffected, ErrorMessage) = await _dbService.CreateNewSalesOrderDetail(salesOrderDetail);
+            if (ErrorMessage != null)
+                return BadRequest(ErrorMessage);
+            return Ok(rowsAffected);
         }
 
         [HttpPost("CreateNewSalesOrderHeader")]
         public async Task<IActionResult> CreateNewSalesOrderHeader(SalesOrderHeader salesOrderHeader)
         {
-            var result = await _dbService.CreateNewSalesOrderHeader(salesOrderHeader);
-            return Ok(result);
+            var (rowsAffected, ErrorMessage) = await _dbService.CreateNewSalesOrderHeader(salesOrderHeader);
+            if (ErrorMessage != null)
+                return BadRequest(ErrorMessage);
+            return Ok(rowsAffected);
         }
 
         [HttpPut("{salesOrderID}/{statusID}")]
-        public async Task UpdateSalesOrderStatus(int salesOrderID, int statusID)
+        public async Task<IActionResult> UpdateSalesOrderStatus(int salesOrderID, int statusID)
         {
-            await _dbService.UpdateSalesOrderStatus(salesOrderID, statusID);
+            var ErrorMessage = await _dbService.UpdateSalesOrderStatus(salesOrderID, statusID);
+            if (ErrorMessage != null)
+                return BadRequest(ErrorMessage);
+            return Ok();
         }
     }
 }

2c2e2ac5ba2a01e1e74f1b589bd20a8c630b58cf
Algirdas Poskus <AlgirdasLeonardas@gmail.com>
2024-06-05 21:39:13 +0300

    Added shipping and billing address fields to SalesOrderHeader model

--- Models/SalesOrderHeader.cs
+++ Models/SalesOrderHeader.cs
@@ -4,6 +4,8 @@ namespace API.Models
     {
         public DateTime DueDate { get; set; } = DateTime.MinValue;
         public int CustomerID { get; set; } = 0;
+        public int? ShipToAddressID { get; set; }
+        public int? BillToAddressID { get; set; }
         public string ShipMethod { get; set; } = string.Empty;
     }
 }

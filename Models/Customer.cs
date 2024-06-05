a4059bff1661126b39ec209734439e1f1e96fe2c
Algirdas Poskus <AlgirdasLeonardas@gmail.com>
2024-06-05 21:32:08 +0300

    Added address fields to Customer model

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -6,5 +6,11 @@ namespace API.Models
         public string LastName { get; set; } = string.Empty;
         public string PasswordHash { get; set; } = string.Empty;
         public string PasswordSalt { get; set; } = string.Empty;
+        public string AddressLine1 { get; set; } = string.Empty;
+        public string? AddressLine2 { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string StateProvince { get; set; } = string.Empty;
+        public string CountryRegion { get; set; } = string.Empty;
+        public string PostalCode { get; set; } = string.Empty;
     }
 }

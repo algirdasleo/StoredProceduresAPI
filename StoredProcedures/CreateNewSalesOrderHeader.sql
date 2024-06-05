a0b4d380eba2e8c3c9f3477bff2bdd575e310ebd
Algirdas Poskus <AlgirdasLeonardas@gmail.com>
2024-06-05 11:25:16 +0300

    Added error handling + optional parameters

--- StoredProcedures/CreateNewSalesOrderHeader.sql
+++ StoredProcedures/CreateNewSalesOrderHeader.sql
@@ -1,9 +1,53 @@
 CREATE PROCEDURE CreateNewSalesOrderHeader
     @DueDate DATETIME,
     @CustomerID INT,
-    @ShipMethod NVARCHAR(50)
+    @ShipToAddressID INT = NULL,   -- Optional parameter
+    @BillToAddressID INT = NULL,   -- Optional parameter
+    @ShipMethod NVARCHAR(50),
+    @ErrorMessage NVARCHAR(4000) OUTPUT
 AS
 BEGIN
-    INSERT INTO SalesLT.SalesOrderHeader (DueDate, CustomerID, ShipMethod)
-    VALUES (@DueDate, @CustomerID, @ShipMethod);
+    IF NOT EXISTS (SELECT 1 FROM SalesLT.Customer WHERE CustomerID = @CustomerID)
+    BEGIN
+        IF @ErrorMessage IS NULL
+        BEGIN
+            SET @ErrorMessage = 'Customer does not exist.';
+        END
+        ELSE 
+        BEGIN
+            SET @ErrorMessage = @ErrorMessage + ' Customer does not exist.';
+        END
+    END
+
+    IF @ShipToAddressID IS NOT NULL AND NOT EXISTS (SELECT 1 FROM SalesLT.Address WHERE AddressID = @ShipToAddressID)
+    BEGIN
+        IF @ErrorMessage IS NULL
+        BEGIN
+            SET @ErrorMessage = 'Shipping address does not exist.';
+        END
+        ELSE
+        BEGIN
+            SET @ErrorMessage = @ErrorMessage + ' Shipping address does not exist.';
+        END
+    END
+
+    IF @BillToAddressID IS NOT NULL AND NOT EXISTS (SELECT 1 FROM SalesLT.Address WHERE AddressID = @BillToAddressID)
+    BEGIN
+        IF @ErrorMessage IS NULL
+        BEGIN
+            SET @ErrorMessage = 'Billing address does not exist.';
+        END
+        ELSE
+        BEGIN
+            SET @ErrorMessage = @ErrorMessage + ' Billing address does not exist.';
+        END
+    END
+
+    IF @ErrorMessage IS NOT NULL
+    BEGIN
+        RETURN;
+    END
+
+    INSERT INTO SalesLT.SalesOrderHeader (DueDate, CustomerID, ShipToAddressID, BillToAddressID, ShipMethod)
+    VALUES (@DueDate, @CustomerID, @ShipToAddressID, @BillToAddressID, @ShipMethod);
 END

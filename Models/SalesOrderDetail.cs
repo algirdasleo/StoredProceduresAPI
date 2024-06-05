4cec63680d6b58f600e8ebdfdf06f9b5bebd4086
Algirdas Poskus <AlgirdasLeonardas@gmail.com>
2024-06-05 21:35:08 +0300

    Add ed UnitPriceDiscount property to SalesOrderDetail model

--- Models/SalesOrderDetail.cs
+++ Models/SalesOrderDetail.cs
@@ -6,5 +6,6 @@ namespace API.Models
         public int OrderQty { get; set; } = 0;
         public int ProductID { get; set; } = 0;
         public decimal UnitPrice { get; set; } = 0;
+        public decimal? UnitPriceDiscount { get; set; } = 0;
     }
 }
